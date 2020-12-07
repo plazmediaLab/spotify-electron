76db8e99a24b4511f2c47694d2730e476c44b8b9
AdrianGD1337 <contacto.plazmedia@gmail.com>
2020-12-07 15:12:46 -0600

    [INIT] Primera configuración del proyecto + Tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const nam = 'Adrian Nieves Uribe';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto p-3">
+      <h1>Create react app - Spotify</h1>
     </div>
   );
 }
