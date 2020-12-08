840d732cfc38f66e32d755cd2a0b91b38a0b392d
AdrianGD1337 <contacto.plazmedia@gmail.com>
2020-12-07 19:38:56 -0600

    [ADD] Página Loading inicial agregada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import firebase from 'utils/Firebase';
 import 'firebase/auth';
+import { useState } from 'react';
+import LoadingIcon from './components/resources/loading-icon';
+import PlazmediaLogo from './design-by-white.svg';
 
 function App() {
+  const [loged, setLoged] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   firebase.auth().onAuthStateChanged((currentUser) => {
-    console.log(currentUser ? 'User logged' : 'Unauthorized');
+    if (!currentUser) {
+      setLoged(null);
+    } else {
+      setLoged(currentUser);
+    }
+    setLoading(false);
   });
+
+  if (loading) {
+    return (
+      <div className="loading-page grid place-items-center w-full h-screen text-center">
+        <section>
+          <LoadingIcon w="60" h="60" classN="mx-auto" fill="#7000f8" />
+          <p className="text-secondary font-medium tracking-wider mt-4">Loading...</p>
+        </section>
+        <footer className="p-5">
+          <img src={PlazmediaLogo} alt="Plazmedia Logo" className="h-10 opacity-40 mr-5" />
+        </footer>
+      </div>
+    );
+  }
 
   return (
     <div className="container mx-auto py-3 px-5 bg-white mt-5 rounded-md bg-background text-secondary">
