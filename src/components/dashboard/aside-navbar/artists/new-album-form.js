83f51caec9d83e924dbdc546a0cf5d74420e3f94
AdrianGD1337 <contacto.plazmedia@gmail.com>
2020-12-30 18:59:44 -0600

    [UI] Trabajando en el selector para el artista

--- src/components/dashboard/aside-navbar/artists/new-album-form.js
+++ src/components/dashboard/aside-navbar/artists/new-album-form.js
@@ -4,6 +4,7 @@ import { useContext, useState } from 'react';
 import AppContext from 'reducer/App/AppContext';
 import AlbumAvatarUpload from './album-avatar-upload';
 import Select from 'react-select';
+import { red } from 'tailwindcss/colors';
 
 export default function NewAlbumForm({ setShow }) {
   const [errorForm] = useState(false);
@@ -13,17 +14,45 @@ export default function NewAlbumForm({ setShow }) {
   const appContext = useContext(AppContext);
   const { artists } = appContext;
 
-  console.log(artists);
-
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ];
+  const customStyles = {
+    singleValue: () => ({
+      color: '#F3F3F9',
+      paddingLeft: '0.25rem'
+    }),
+    input: () => ({
+      color: '#F3F3F9'
+    }),
+    indicatorSeparator: (styles) => ({
+      ...styles,
+      backgroundColor: '#494A4E'
+    }),
+    control: (styles, { isFocused, isSelected }) => ({
+      ...styles,
+      boxShadow: 'none',
+      border: 'none',
+      backgroundColor: isFocused ? '#494A4E' : '#16171B',
+      borderRadius: '0.25rem'
+    }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      // const color = chroma(data.color);
+      return {
+        ...styles,
+        backgroundColor: isSelected ? '#7000f8' : '',
+        cursor: isDisabled ? 'not-allowed' : 'default',
+        ':hover': {
+          backgroundColor: '#dbbffd'
+        }
+      };
+    },
+    placeholder: (styles, { isFocused }) => ({
+      ...styles,
+      color: isFocused ? '#F3F3F9' : '#494A4E'
+    })
+  };
 
   return (
     <form>
-      <section className="grid grid-cols-auto-1fr gap-3">
+      <section className="grid grid-cols-auto-1fr gap-3 out">
         <AlbumAvatarUpload coverUrl={coverUrl} setCoverUrl={setCoverUrl} />
         <FormInputModal
           placeholder="Nombre del album"
@@ -31,6 +60,16 @@ export default function NewAlbumForm({ setShow }) {
           // errmessage="Test of error message"
         />
         <Select
+          styles={customStyles}
+          // theme={(theme) => ({
+          //   ...theme,
+          //   borderRadius: '0.25rem',
+          //   colors: {
+          //     ...theme.colors,
+          //     primary25: '#dbbffd',
+          //     primary: '#7000f8'
+          //   }
+          // })}
           options={artists}
           className="text-background"
           isLoading={false}
